3d2d56bcf7e9fbcd20ff44e5e620a0fb7e2b90ea
wargen <wargenng@gmail.com>
2019-11-13 08:46:08 -0800

    proj9

--- Prog.Lang/proj9.go
+++ Prog.Lang/proj9.go
@@ -1,9 +1,33 @@
 package main
 
-//"io"
-//"strings"
-//"os"
-//"time"
-//"path"
-//"net/http"
-//"golang.org/x/net/html"
+import (
+	"fmt"
+	// "io"
+	// "strings"
+	// "os"
+	// "time"
+	// "path"
+	// "net/http"
+	//"golang.org/x/net/html"
+)
+
+urlList := []string {
+	"https://www.unlv.edu/cs",
+	"https://www.unlv.edu/engineering",
+	"https://www.unlv.edu/engineering/advising-center",
+	"https://www.unlv.edu/engineering/about",
+	"https://www.unlv.edu/engineering/academic-programs",
+	"https://www.unlv.edu/ceec",
+	"https://ece.unlv.edu/",
+	"https://www.unlv.edu/me",
+	"https://www.unlv.edu/rotc",
+	"https://www.unlv.edu/afrotc",
+	"https://www.unlv.edu/eed",
+	"https://www.unlv.edu/engineering/mendenhall",
+	"https://www.unlv.edu/engineering/uas",
+	"https://www.unlv.edu/engineering/solar",
+	"https://www.unlv.edu/engineering/techcommercialization",
+	"https://www.unlv.edu/engineering/railroad",
+	"https://www.unlv.edu/engineering/future-students",
+	"https://www.physics.unlv.edu/",
+}
