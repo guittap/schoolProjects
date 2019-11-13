bbe028530bd1745e55082a171d74110954d16b69
wargen <wargenng@gmail.com>
2019-11-12 20:52:33 -0800

    project9 notes
    uploaded the notes for project 9

--- Prog.Lang/proj9.go
+++ Prog.Lang/proj9.go
@@ -1,73 +1,9 @@
 package main
 
-import (
-	"fmt"
-	"io"
-	"strings"
-	"os"
-	"time"
-	"path"
-	"net/http"
-	"golang.org/x/net/html"
-)
-
-urlList := []string {
-	"https://www.unlv.edu/cs",
-	"https://www.unlv.edu/engineering",
-	"https://www.unlv.edu/engineering/advising-center",
-	"https://www.unlv.edu/engineering/about",
-	"https://www.unlv.edu/engineering/academic-programs",
-	"https://www.unlv.edu/ceec",
-	"https://ece.unlv.edu/",
-	"https://www.unlv.edu/me",
-	"https://www.unlv.edu/rotc",
-	"https://www.unlv.edu/afrotc",
-	"https://www.unlv.edu/eed",
-	"https://www.unlv.edu/engineering/mendenhall",
-	"https://www.unlv.edu/engineering/uas",
-	"https://www.unlv.edu/engineering/solar",
-	"https://www.unlv.edu/engineering/techcommercialization",
-	"https://www.unlv.edu/engineering/railroad",
-	"https://www.unlv.edu/engineering/future-students",
-	"https://www.physics.unlv.edu/",
-}
-
-resp, err := http.Get(url)
-if err != nil {
-	return err
-}
-defer resp.Body.Close()
-
-bdy := resp.Body
-tkn := html.NewTokenizer(bdy)
-
-for {
-	currTkn = tkn.Next()
-	switch {
-	case currTkn == html.ErrorToken:
-		return
-	}
-}
-
-start := time.Now() 
-///
-///\/
-//\\stuff
-//\/\/\/
-//\\//\\/\/\/\/||/\/||//\/\||/\||/\||/\||
-
-if err := DownloadFile ("file.jpg", "https://www.unlv.edu")
-
-file, err := os.Create("foundImages.txt")
-if err != nil {
-	//...
-}
-defer file.Close()
-_, err == file.writeString("Hello, World")
-if err := nil {
-	//...
-}
-
-for _, url := range seedUrls {
-	go myScrapper
-}
+//"io"
+//"strings"
+//"os"
+//"time"
+//"path"
+//"net/http"
+//"golang.org/x/net/html"
