1bbda8a946fa9b3c3940531e7e2d3aaee19e59a1
wargen <wargenng@gmail.com>
2020-02-04 14:41:30 -0800

    Update .gitignore

--- S20/CS458/hw1/hw1.min.py
+++ S20/CS458/hw1/hw1.min.py
@@ -1,13 +1,10 @@
 import matplotlib.pyplot as p
 data = open(r"myData.txt", "r")
-x = []
-y = []
-for line in data:
-    x.append(int(line.split(",")[0]))
-    y.append(int(line.split(",")[1]))
+x = [int(line.split(",")[0]) for line in data]
+y = [int(line.split(",")[1]) for line in data]
 length = len(x)
-slope = ((sum(y)/length) - (sum(x[i]*y[i] for i in range(length))/length) / (sum(x)/length)) / (
-    (sum(x)/length) - (sum(x[i]**2 for i in range(length))/length) / (sum(x)/length))
+slope = ((sum(y)/length) - (sum([x[i]*y[i] for i in range(length)])/length) / (sum(x)/length)) / (
+    (sum(x)/length) - (sum([x[i]**2 for i in range(length)])/length) / (sum(x)/length))
 yIntercept = (sum(y)/length) - slope * (sum(x)/length)
 p.plot([min(x), max(x)], [slope*min(x) + yIntercept, slope*max(x)+yIntercept])
 p.plot(x, y, "o")
