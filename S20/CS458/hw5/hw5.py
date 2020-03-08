f1cb2e18e5a4e357d7f66fcf2a7f73975257cec3
wargen <wargenng@gmail.com>
2020-03-08 15:32:17 -0700

    hw5 final fixed with new test and train data

--- S20/CS458/hw5/hw5.py
+++ S20/CS458/hw5/hw5.py
@@ -7,17 +7,21 @@ from sklearn.metrics import accuracy_score
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
-from hw4 import split_data
 
 
 def model():
-    df = pd.read_csv('mtcars.csv', header=0, index_col=False)
-    am = list(df['am'])
-    df.drop(['car_model', 'hp', 'am'], axis=1, inplace=True)
-    scaler = MinMaxScaler()
-    df = scaler.fit_transform(df)
+    X_train = pd.read_csv('train.csv', header=0, index_col=False)
+    y_train = list(X_train['am'])
+    X_train.drop(['car_model', 'hp', 'am', 'Unnamed: 12'],
+                 axis=1, inplace=True)
 
-    X_train, X_test, y_train, y_test = split_data(df, am)
+    X_test = pd.read_csv('test.csv', header=0, index_col=False)
+    y_test = list(X_test['am'])
+    X_test.drop(['car_model', 'hp', 'am', 'Unnamed: 12'], axis=1, inplace=True)
+
+    scaler = MinMaxScaler()
+    X_train = scaler.fit_transform(X_train)
+    X_test = scaler.fit_transform(X_test)
 
     lr = LogisticRegression()
     lr.fit(X_train, y_train)
