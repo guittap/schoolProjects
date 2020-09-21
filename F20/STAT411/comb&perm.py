4cf93db37805897cdabda860d746ea6fb468e1fb
Wargen Guittap <48695406+guittap@users.noreply.github.com>
2020-09-20 18:13:49 -0700

    chapter 6 homework

---
+++ F20/STAT411/comb&perm.py
@@ -0,0 +1,88 @@
+import math
+
+# print(
+#     (math.comb(16,1)*math.comb(15,3)) +
+#     (math.comb(16,2)*math.comb(15,2)) +
+#     (math.comb(16,3)*math.comb(15,1)) +
+#     (math.comb(16,4)*math.comb(15,0))
+
+# )
+
+#finding coin then dice then deck of cards
+# print(
+#     2**5 * 6**2 * math.comb(52,4)
+# )
+
+# songs for playlist and order of songs
+# print(
+#     math.comb(11,2) * math.comb(16,2) * math.comb(19,2) * math.perm(6,6)
+# )
+
+# more dumb playlist stuff
+# print(
+#     math.perm(12,6),
+#     12*11*10*9*8*7,
+#     math.comb(12,6) * math.perm(6,6)
+# )
+
+# its pizza time
+# print(
+#     math.comb(4,1)*math.comb(5,3)*math.comb(7,5)
+# )
+
+# # choose shit
+# print(
+#     math.comb(25,3)
+# )
+
+# choose and permute shit
+# print (
+#      math.perm(12,8) /math.comb(12,6)
+# )
+# print (
+#      math.comb(11,9) /math.perm(11,6)
+# )
+
+# You are getting a line-up ready for a school kickball game.  
+# You have 4 girls and 4 boys.  
+# The rules state each child must kick the same number of times and alternate girl-boy or boy-girl.  
+# How many ways can a line-up be made for one round of kicking?
+# print(
+#     math.perm(5,5) * math.perm(5,5) * 2
+# )
+
+# shitty movie genre's
+# print(
+#     (math.comb(15,2)*math.comb(10,1)) +
+#     (math.comb(15,1)*math.comb(10,2)) +
+#     (math.comb(15,0)*math.comb(10,3)) 
+
+# )
+
+# weird coin problem
+# print(
+#     math.comb(11,6) + math.comb(11,7) + math.comb(11,8)
+# )
+
+# perm
+# print (
+#     math.perm(12,4)
+# )
+
+ #longe problem
+# print(
+#     (math.comb(16,10)*math.comb(6,0) +
+#     math.comb(16,9)*math.comb(6,1) +
+#     math.comb(16,8)*math.comb(6,2) +
+#     math.comb(16,7)*math.comb(6,3) +
+#     math.comb(16,6)*math.comb(6,4) ) * math.perm(10,10)
+# )
+
+# print(math.comb(16,4)*math.comb(6,6) * math.perm(10,10))
+
+
+# flowers
+# print(
+#     math.comb(10,4)* math.comb(6,2) * math.comb(10,2)
+# )
+
